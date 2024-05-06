c3ec9cc9778c3887adfbe8fbc445fcdc79295566
kubraaygun <kubraaygun@outlook.com.tr>
2024-05-06 11:04:57 +0300

    components olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import List from "./components/List";
+
 const App = () => {
-  return <div>App</div>;
+  const [users, setUsers] = useState([
+    {
+      name: "Mehmet",
+      mail: "mehmet60@gmail.com",
+      age: 60,
+    },
+    {
+      name: "Ali",
+      mail: "alihttp@gmail.com",
+      age: 30,
+    },
+  ]);
+  return (
+    <div className="container">
+      <h2 className=" text-center my-5">Kullanici Paneli</h2>
+      <Form />
+      <List users={users} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/List/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const List = ({ users }) => {
+  return (
+    <table className="table my-4 table-dark table-responsive table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Isim</th>
+          <th>Email</th>
+          <th>Yas</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr>
+            <td>{user.name}</td>\<td>{user.mail}</td>
+            <td>{user.age}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default List;
